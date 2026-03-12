ce08d0010b78fd68cd1c756e6b625f16a79b77b2
arun-sekar <arunsmiracle@gmail.com>
2026-03-12 23:38:10 +0000

    fix: main execution

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"github.com/aroonsekar/dbsnap/cmd"
 )
 
 func main() {
-	if err := cmd.Execute(); err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	cmd.Execute()
 }
